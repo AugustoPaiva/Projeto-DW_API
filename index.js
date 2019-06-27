be509570751b6cd04f23f5c5d6ffd5bb47719aa5
AugustoPaiva <augustopaiva4@gmail.com>
2019-06-27 00:35:52 -0300

    adding cors

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require("express");
+var bodyParser = require("body-parser");
+
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/api", require("./rotas"));
 
